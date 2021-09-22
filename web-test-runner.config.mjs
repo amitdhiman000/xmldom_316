97904f43c9f887d63e787c725c9708e0cdc74487
Amit Dhiman <amitdhiman000@gmail.com>
2021-09-22 14:35:00 +0530

    Fixed the esm module import issue

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,3 +1,13 @@
+import rollupCommonjs  from '@rollup/plugin-commonjs';
+import { fromRollup } from '@web/dev-server-rollup';
+
+const commonjs = fromRollup(rollupCommonjs);
+
 export default {
     nodeResolve: true,
+    plugins: [
+        commonjs({
+            include: ["**/node_modules/@xmldom/xmldom/lib/*.js"]
+        })
+    ]
 };
